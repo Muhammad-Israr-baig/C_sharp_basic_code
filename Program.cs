fd9083011fa18ebb36ac565c9b9ea74ee4f9b157
M.Israr <israrbaig557799@gmail.com>
2025-04-11 20:40:30 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+  namespace Mcode
+  {
+    
+    class Pratices{   
+           public static void Main(string[] args){
+            int a,b;
+
+            System.Console.WriteLine("Enter the value of a & b :");
+            a=Convert.ToInt32(Console.ReadLine());
+            b=Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine($"The sum of {a} and {b} is {a+b}");
+
+           Console.ReadKey();   
+           }
+    }
+  }
